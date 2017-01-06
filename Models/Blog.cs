c3e39fd32653b7d56075fca8b73d2b415556ea4c
Jessica Prieto <jessicaprieto31@gmail.com>
2017-01-06 16:18:32 -0700

    add id property to Blog

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -2,7 +2,9 @@ using System;
 
 namespace BlogApi.Models {
     public class BlogPost {
+        public string Id { get; set; }
         public string Title { get; set; }
-    
+        public DateTime DatePosted { get; set; }
+        public string Content { get; set; }
     }
 }
