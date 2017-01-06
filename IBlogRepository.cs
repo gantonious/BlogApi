87f73b9aaa3fe7f0b0ac88ca49758592831dc694
Jessica Prieto <jessicaprieto31@gmail.com>
2017-01-06 16:11:57 -0700

    added blog repository interface

---
+++ IBlogRepository.cs
@@ -0,0 +1,9 @@
+using BlogApi.Models;
+using System.Collections.Generic;
+
+namespace BlogApi {
+    public interface IBlogRepository {
+        IEnumerable<BlogPost> GetAll();
+        void CreatePost(BlogPost blogPost);
+    }
+}
