fa388c11b190bc573d7a5f70b55b7d89fd787314
Jessica Prieto <jessicaprieto31@gmail.com>
2017-01-06 16:44:39 -0700

    implemented InMemoryBlogRepository

---
+++ InMemoryBlogRepository.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using BlogApi.Models;
+
+namespace BlogApi {
+    public class InMemoryBlogRepository : IBlogRepository
+    {
+        private Dictionary<string, BlogPost> blogDictionary = new Dictionary<string, BlogPost>();
+        public void CreatePost(BlogPost blogPost)
+        {
+            blogDictionary[blogPost.Id] = blogPost;
+        }
+
+        public void DeletePost(string blogId)
+        {
+            blogDictionary.Remove(blogId);
+        }
+
+        public IEnumerable<BlogPost> GetAll()
+        {
+            return blogDictionary.Values;
+        }
+    }
+}
